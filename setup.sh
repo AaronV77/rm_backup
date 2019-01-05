3a10a9ef1b72c79fb5436c06efb43b7143c644a1
AaronV77 <aaronvaloroso77@gmail.com>
2019-01-04 21:58:41 -0600

    The First commit

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Get the time duration on how long the user wants the files to persist.
+    # Year
+    # Month
+    # Day
+    # Hours
+    # Minutes
+    # Seconds
+# Calculate the total number of seconds from the given input.
+# Find the location in the rm alias file and replace it with the total seconds.
+
+# Find the absolute path of the rm command.
+# Find the location of rm in the alias file and replace it with the absolute path.
+    # Don't replace the rm command name of the alias with the absolute path.
+    # Skp this one.
+
+# Find what line the rm alias in either the .bashrc or .bash_profile.
+# Delete the next following line to remove the whole alias. (IDK How Yet)
+# Add the alias to the end of either the .bashrc and .bash_profile.
