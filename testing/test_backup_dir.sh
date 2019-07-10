1b2036061669be3a3bbb47d80d268e858ec105fe
Aaron  Valoroso <aaron.a.valoroso@erdc.dren.mil>
2019-07-09 21:56:10 -0500

    Minor Change-15.

--- testing/test_backup_dir.sh
+++ testing/test_backup_dir.sh
@@ -3,6 +3,7 @@ rm something.txt
 
 if [ ! -d $BACKUP ]; then 
     ls -la $HOME
+    cat $HOME/.bashrc
     echo "The rm_backup directory could not be found in the '$HOME' directory."
     exit 1
 else 
