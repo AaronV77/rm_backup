5d00c1295f99d24d09db5e9ea08b428bf13d12b7
Aaron  Valoroso <aaron.a.valoroso@erdc.dren.mil>
2019-07-10 21:40:50 -0500

    Minor Change-43.

--- testing/test_3_file_removal.sh
+++ testing/test_3_file_removal.sh
@@ -1,11 +1,8 @@
 touch something.txt
 $RUN something.txt
 
-cd $BACKUP
-directory_contents=(*)
-ls -la $BACKUP/backup
 /bin/rm -rf $BACKUP/backup/*
-echo ${#directory_contents[@]}
+number_of_files=$(ls -1 $BACKUP/backup| wc -l)
 
 if [ ${#directory_contents[@]} != 1 ]; then
     echo "There was something wrong with the contents of .rm_backup."
