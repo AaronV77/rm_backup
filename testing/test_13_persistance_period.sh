a6178e4f4d38a055835fe96384eae272f6b74521
Aaron  Valoroso <aaron.a.valoroso@erdc.dren.mil>
2019-07-11 00:01:53 -0500

    Minor Change-50.

--- testing/test_13_persistance_period.sh
+++ testing/test_13_persistance_period.sh
@@ -1,7 +1,7 @@
 touch something_1.txt
 $RUN something_1.txt
 
-sleep 1m 10s
+sleep 2m
 
 touch something_2.txt
 $RUN something_2.txt
@@ -10,6 +10,7 @@ ls $BACKUP/backup
 number_of_files=$(ls -1 $BACKUP/backup | wc -l)
 /bin/rm -rf $BACKUP/backup/*
 echo "HERE: $number_of_files"
+
 if [ $number_of_files != 1 ]; then
     echo "The persistance time is not removing old files / folders from the backup."
     exit 1
