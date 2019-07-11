ed57b212b2322a6b971ccfb3e2a1fac2ee163967
Aaron  Valoroso <aaron.a.valoroso@erdc.dren.mil>
2019-07-10 21:44:33 -0500

    Minor Change-43.

--- testing/test_3_file_removal.sh
+++ testing/test_3_file_removal.sh
@@ -3,7 +3,7 @@ $RUN something.txt
 
 /bin/rm -rf $BACKUP/backup/*
 number_of_files=$(ls -1 $BACKUP/backup| wc -l)
-
+echo $number_of_files
 if [ $number_of_files != 1 ]; then
     echo "There was something wrong with the contents of .rm_backup."
     exit 1
