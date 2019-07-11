90eba7e28e468caa9c0e8650bb5e61a1cf506b89
Aaron  Valoroso <aaron.a.valoroso@erdc.dren.mil>
2019-07-11 11:21:06 -0500

    Minor Change-63.

--- tear_down.sh
+++ tear_down.sh
@@ -1,11 +1,11 @@
 
 occurences=$(grep -o "rm ()" $HOME/.bashrc | wc -l)
 echo "Number of occurences: $occurences"
-if [ $occurences > 0 ]; then
-    if [ $occurences == 1 ]; then
+if [ $occurences -gt 0 ]; then
+    if [ $occurences -eq 1 ]; then
         line_number=$(grep -nr "rm ()" $HOME/.bashrc | cut -d: -f1)
         sed -i $line_number'd' $HOME/.bashrc
-    elif [ $occurences > 1 ]; then
+    elif [ $occurences -gt 1 ]; then
         if [[ "$OSTYPE" == "linux-gnu" ]]; then
             echo "Your .bashrc is littered with rm () aliases, please clean up."
         elif [[ "$OSTYPE" == "darwin"* ]]; then 
