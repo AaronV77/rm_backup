beac3294a1c7681a79e8da48154cf84d7270263d
Aaron  Valoroso <aaron.a.valoroso@erdc.dren.mil>
2019-07-11 10:12:29 -0500

    Minor Change-51.

--- testing/test_14_star_folder_removal.sh
+++ testing/test_14_star_folder_removal.sh
@@ -6,15 +6,17 @@ cd ..
 $RUN -rf something/*
 number_of_files=$(ls -1 $BACKUP/backup | wc -l)
 /bin/rm -rf $BACKUP/backup/*
-
+echo "CHECK-1"
 if [ $number_of_files != 2 ]; then
     echo "The folder did not get removed/backed up correctly."
     exit 1
 fi
-
+echo "CHECK-2"
 if [ ! -d something ]; then
     echo "The existing folder did not stay around after deleteing everything in the folder."
     $RUN -rf something
     exit 1
 fi
+echo "CHECK-3"
 $RUN -rf something
+echo "CHECK-4"
