fed33f19202a96655b77f67f50800f7f863e5274
Aaron  Valoroso <aaron.a.valoroso@erdc.dren.mil>
2019-07-10 23:29:33 -0500

    Minor Change-44.

--- testing/test_7_multiple_file_removal.sh
+++ testing/test_7_multiple_file_removal.sh
@@ -1,5 +1,5 @@
 touch something_1.txt something_2.txt something_3.txt
-$RUN something.txt something_2.txt something_3.txt
+$RUN something_1.txt something_2.txt something_3.txt
 number_of_files=$(ls -1 $BACKUP/backup| wc -l)
 /bin/rm -rf $BACKUP/backup/*
 if [ $number_of_files != 3 ]; then
